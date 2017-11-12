c75be4a3d1d6c73d96ebabc2934a60f9aaba87d5
YuliiaVovk <dp100193vjj@gmail.com>
2017-11-12 19:21:07 +0200

    Add files via upload

--- new-summ-function/every-function.js
+++ new-summ-function/every-function.js
@@ -1,24 +1,17 @@
-function every (){
-	'use strict';
+//[2, 4, 6].every(isEven) // true
+//[1, 2, 3].every(isEven) // false
 
-	function isEven(element, index, array) {
-		if (element % 2 === 0) return true;
-		else return false;
+function isEven(value, index, ar){
+		'use strict';
+	if (value % 2 == 0)
+		return true;
+	else
+		return false;
+}
+function every(numbers,isEven){
+if (numbers.every(isEven))
+	return true;
+else
+	return false;
 };
-	return arguments[i].every(isEven);
-	return Array.prototype.every(arguments, function isEven {
-}, 0);
-};
-
-every ([2, 4, 6],isEven);
-
-
-
-
-function isEven(element, index, array) {
-		if (element % 2 === 0) return true;
-		else return false;
-};
-
-[2, 4, 6].every(isEven) // true
-[1, 2, 3].every(isEven) // false
+every([2, 4, 6],isEven);
